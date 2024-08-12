28173f8246c1dd487417faa4c5727e4ad79be0b1
diegoe120 <38639302+diegoe120@users.noreply.github.com>
2024-08-12 12:40:26 -0300

    corregido el error de http/s

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import LoadingButton from '@mui/lab/LoadingButton';
 import { Container, Typography, Box, TextField } from "@mui/material"
 import { useState } from "react";
 
-const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=`;
+const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=`;
 
 export default function App() {
 
